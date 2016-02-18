a2899942fe20f57d54be0117b95f3d5bc12d766a
Himzo Hasak <hasak97@hotmail.com>
2016-02-18 13:11:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Uno
+ Professional online platform for an official Škoda partner, designed to provide customers with seamless access to vehicle information, services, and dealership offerings.
---
+++ js/navigation.js
@@ -0,0 +1,117 @@
+// pocetna
+	$(".pocetna").click( function() {
+		window.location = "index.html";
+	});
+// autoSkola
+	$(".autoSkola").click( function() {
+		window.location = "autoSkola.html";
+	});
+// novosti
+	$(".novosti").click( function() {
+		window.location = "index.html";
+	});
+	//nije dovršeno !!
+// oNama
+	$(".oNama").click( function() {
+		window.location = "oNama.html";
+	});
+// kontakt
+	$(".kontakt").click( function() {
+		window.location = "kontakt.html";
+	});
+// akcije
+	$(".akcije").click( function() {
+		window.location = "http://www.skoda.ba/";
+	});
+	//nije dovršeno !!
+// servisnaSluzba
+	$(".servisnaSluzba").click( function() {
+		window.location = "index.html";
+	});
+	//nije dovršeno !!
+// finansiranje
+	$(".finansiranje").click( function() {
+		window.location = "http://www.skoda.ba/savjeti-kupovina/finansiranje";
+	});
+	//nije dovršeno !!
+// multimedija
+	$(".multimedija").click( function() {
+		window.location = "index.html";
+	});
+	//nije dovršeno !!
+// konfigurator
+	$(".konfigurator").click( function() {
+		window.location = "http://www.skoda.ba/";
+	});
+	//nije dovršeno !!
+// citigo
+	$(".citigo").click( function() {
+		window.location = "citigo.html";
+	});
+// fabia
+	$(".fabia").click( function() {
+		window.location = "fabia.html";
+	});
+// fabiaMonteCarlo
+	$(".fabiaMonteCarlo").click( function() {
+		window.location = "fabiaMonteCarlo.html";
+	});
+// fabiaCombi
+	$(".fabiaCombi").click( function() {
+		window.location = "fabiaCombi.html";
+	});
+// rapid
+	$(".rapid").click( function() {
+		window.location = "rapid.html";
+	});
+// rapidSpaceback
+	$(".rapidSpaceback").click( function() {
+		window.location = "rapidSpaceBack.html";
+	});
+// octavia
+	$(".octavia").click( function() {
+		window.location = "octavia.html";
+	});
+// octaviaRS
+	$(".octaviaRS").click( function() {
+		window.location = "octaviaRs.html";
+	});
+// octaviaCombi
+	$(".octaviaCombi").click( function() {
+		window.location = "octaviaCombi.html";
+	});
+// octaviaCombiRS
+	$(".octaviaCombiRS").click( function() {
+		window.location = "octaviaCombi.html";
+	});
+// octaviaScout
+	$(".octaviaScout").click( function() {
+		window.location = "octaviaScout.html";
+	});
+// yeti
+	$(".yeti").click( function() {
+		window.location = "yeti.html";
+	});
+// superb
+	$(".superb").click( function() {
+		window.location = "superb.html";
+	});
+// superbCombi
+	$(".superbCombi").click( function() {
+		window.location = "superb.html";
+	});
+// roomster
+	$(".roomster").click( function() {
+		window.location = "yeti.html";
+	});
+// rabljenaVozila
+	$(".rabljenaVozila").click( function() {
+		window.location = "http://www.olx.ba/profil/autokucauno";
+	});
+
+	//sakriveni linkovi dok se CMS ne napravi
+
+	$(".novosti").hide();
+
+	$(".servisnaSluzba").hide();
+	$(".multimedija").hide();
